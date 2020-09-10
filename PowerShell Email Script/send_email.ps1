def4b20ed3cc5875846f40cc5aaa0da616b7c071
Lee Murray <mrleemurray@gmail.com>
2020-09-10 10:58:22 +0100

    Update send_email.ps1
    Show list of available templates before asking for filename

--- PowerShell Email Script/send_email.ps1
+++ PowerShell Email Script/send_email.ps1
@@ -1,9 +1,12 @@
-Param (
-    [string]$HTMLFile = $( Read-Host "Filename (include '.html')" ),
-    [string]$Subject = $( Read-Host "Email subject: " ),
-    [string]$Recipient = $( Read-Host "Who do you want to send the email to? :" )
-)
+# Get a list of all .html files in the current directory
 
+$Dir = Get-ChildItem .
+$List = $Dir | where {$_.Extension -eq ".html"}
+ Write-Host "Available templates: $List" -ForegroundColor Green
+
+ $HTMLFile = $( Read-Host "Filename (include '.html')" )
+ $Subject = $( Read-Host "Email subject: " )
+ $Recipient = $( Read-Host "Who do you want to send the email to? :" )
 # Check if the personalised email template exists before continuing.
 
 Try {
@@ -19,7 +22,7 @@ Try {
 Try {
     $Outlook = New-Object -com Outlook.Application
 } Catch {
-    Write-Host "ERROR: Cannot create a new Outlook object. Outlook may not be installed on your machine."
+    Write-Host "ERROR: Cannot create a new Outlook object. Outlook may not be installed on your machine." -ForegroundColor Red
     Break
 }
 
@@ -29,5 +32,5 @@ $Mail.Subject = $Subject
 $Mail.HTMLBody =$HTML
 Write-Output "Sending email to: $Recipient"
 $Mail.Send()
-Write-Output "All done :)"
+Write-Host "All done :)" -ForegroundColor Green
 $outlook.Quit()
